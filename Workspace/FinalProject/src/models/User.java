65d1e4ea309131ebaeece95e3db366b316415dcf
AustinJenkins <AJenkins@student.neumont.edu>
2017-02-28 15:21:19 -0700

    Initial Files

---
+++ Workspace/FinalProject/src/models/User.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.ArrayList;
+
+public class User {
+
+	private String name;
+	private ArrayList<String> blockedWebsites;
+	private Schedule schedule;
+	private BankAccount account;
+
+}
